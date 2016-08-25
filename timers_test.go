64d3ff5ee9bede358133b8b0f899cb6e22275ff0
eugene <eugenemcmad@gmail.com>
2016-08-25 11:31:32 +0300

    +

--- timers_test.go
+++ timers_test.go
@@ -9,17 +9,19 @@ import (
 func TestAfter(t *testing.T) {
 
 	c := time.After(5 * time.Second)
-	for i := 0; i < 100; i++ {
+	go sWatch1(c)
+	for i := 0; i < 300; i++ {
 		time.Sleep(100 * time.Millisecond)
 		fmt.Print(".")
 	}
 
-	fmt.Println("\n end")
+	fmt.Println("\n exit")
+}
+
+func sWatch1(c <-chan time.Time) {
 
 	select {
 	case <-c:
 		fmt.Println("\n timed out")
 	}
-
-	fmt.Println("\n exit")
 }
