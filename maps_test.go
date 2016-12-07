c215e6b7feeb523ef46598917120a64ed83beda1
eugene <eugenemcmad@gmail.com>
2016-12-07 14:27:57 +0300

    +

--- maps_test.go
+++ maps_test.go
@@ -1,6 +1,7 @@
 package tests
 
 import (
+	"errors"
 	"fmt"
 	"testing"
 )
@@ -10,4 +11,68 @@ func TestIsMapIsReference(t *testing.T) {
 	m2 := m1
 	m2[2] = 20
 	fmt.Println(m1)
+}
+
+func TestErrSumMaps(t *testing.T) {
+	pref := "ABOUT_ME:"
+	e1 := errors.New("err1")
+	e2 := errors.New("err2")
+	e3 := errors.New("err3")
+
+	//var es = make(errInfSum)
+	//var es = errInfSum{}
+	var es errInfSum
+
+	es.save(pref)
+
+	es.add(e1)
+	es = errInfSum{}
+
+	es.add(e1)
+	es = newErrInfSum()
+
+	es.add(e1)
+	es.add(e2)
+	es.add(e2)
+	es.add(e3)
+	es.add(e3)
+	es.add(e3)
+	es.save(pref)
+
+	es.add(e2)
+	es.add(e3)
+	es.add(e3)
+	es.save(pref)
+}
+
+// Error description stack with counter. No thread safe.
+type errInfSum struct {
+	m map[string]int
+}
+
+// Get new errors info summary
+func newErrInfSum() errInfSum {
+	return errInfSum{m: make(map[string]int)}
+}
+
+// Add error description.
+func (es *errInfSum) add(e error) {
+	if es.m == nil {
+		es.m = make(map[string]int)
+	}
+
+	var s = e.Error()
+	es.m[s] += 1
+}
+
+// Save to log and reset.
+func (es *errInfSum) save(pref string) {
+	if len(es.m) > 0 {
+		var sum string
+		for s, n := range es.m {
+			sum += fmt.Sprintf("\n\t %d: %s", n, s)
+		}
+		fmt.Printf("%s errors summary:%v\n", pref, sum)
+		es.m = make(map[string]int)
+	}
 }
--- sort_test.go
+++ sort_test.go
@@ -19,6 +19,58 @@ func TestRnd1(t *testing.T) {
 
 }
 
+// Test gnome sort algorithm
+func TestGnomeSortAsc(t *testing.T) {
+	ii := []int{0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 50, 100, -1, -10, -100, -1000}
+
+	rand.Seed(time.Now().UnixNano())
+	a := rndInt(ii)
+	fmt.Println(a)
+	a = sortAsc(a)
+	fmt.Println(a)
+}
+
+// Test gnome sort algorithm
+func TestGnomeSortDsc(t *testing.T) {
+	ii := []int{0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 50, 100, -1, -10, -100, -1000}
+
+	rand.Seed(time.Now().UnixNano())
+	a := rndInt(ii)
+	fmt.Println(a)
+	a = sortDsc(a)
+	fmt.Println(a)
+}
+
+func sortAsc(a []int) []int {
+	i := 1
+	for i < len(a) {
+		if i == 0 || a[i-1] <= a[i] {
+			i++
+		} else {
+			temp := a[i]
+			a[i] = a[i-1]
+			a[i-1] = temp
+			i--
+		}
+	}
+	return a
+}
+
+func sortDsc(a []int) []int {
+	i := 1
+	for i < len(a) {
+		if i == 0 || a[i-1] >= a[i] {
+			i++
+		} else {
+			temp := a[i]
+			a[i] = a[i-1]
+			a[i-1] = temp
+			i--
+		}
+	}
+	return a
+}
+
 func rndInt(ii []int) []int {
 	jj := make([]int, len(ii))
 	xx := rand.Perm(len(ii))
