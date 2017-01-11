23d90ff08eb13131014859b7699a439e68c6490f
eugene <eugenemcmad@gmail.com>
2017-01-11 12:59:31 +0300

    +

--- replace_test.go
+++ replace_test.go
@@ -1,14 +1,19 @@
 package tests
 
 import (
+	"bytes"
 	"html/template"
 	"os"
 	"regexp"
+	"strings"
 	"testing"
+
+	"github.com/alexflint/go-restructure/regex"
+	"github.com/dlclark/regexp2"
 )
 
-// 1000000	2280 ns/op	2.314s
-func BenchmarkTemplate(b *testing.B) {
+// 1000000	2280 ns/op
+func BenchmarkTemplateReplace(b *testing.B) {
 	tmpl := template.New("tt")
 	template.Must(tmpl.Delims("${", "}").Parse("azaza ${ .Name } zazaz. "))
 	for i := 0; i < b.N; i++ {
@@ -16,10 +21,81 @@ func BenchmarkTemplate(b *testing.B) {
 	}
 }
 
-// 1000000	1250 ns/op	1.282s
-func BenchmarkRx(b *testing.B) {
+// 1000000	1216 ns/op
+func BenchmarkRxReplaceAllString(b *testing.B) {
 	var rx = regexp.MustCompile(`\$\{([\w]{1,1000})\}`)
 	for i := 0; i < b.N; i++ {
 		rx.ReplaceAllString(`azaza ${Name} zazaz. `, `Mary`)
+	}
+}
+
+// 1000000	1206 ns/op
+func BenchmarkRxReplaceAll(b *testing.B) {
+	var rx = regexp.MustCompile(`\$\{([\w]{1,1000})\}`)
+	z := []byte(`azaza ${Name} zazaz. `)
+	v := []byte(`Mary`)
+	for i := 0; i < b.N; i++ {
+		rx.ReplaceAll(z,v)
+	}
+}
+
+// 10000000	239 ns/op
+func BenchmarkStringsReplace(b *testing.B) {
+	k := `Name`
+	for i := 0; i < b.N; i++ {
+		k2 := `${` + k + `}`
+		strings.Replace(`azaza ${Name} zazaz. `, k2, `Mary`, -1)
+	}
+}
+
+// 10000000	153 ns/op
+func BenchmarkBytesReplace(b *testing.B) {
+	k := `Name`
+	for i := 0; i < b.N; i++ {
+		k2 := []byte(`${` + k + `}`)
+		bytes.Replace([]byte(`azaza ${Name} zazaz. `), k2, []byte(`Mary`), -1)
+	}
+}
+
+// 10000000	135 ns/op
+func BenchmarkBytesPreReplace(b *testing.B) {
+	z := []byte(`azaza ${Name} zazaz. `)
+	k := `Name`
+	v := []byte(`Mary`)
+	for i := 0; i < b.N; i++ {
+		k2 := []byte(`${` + k + `}`)
+		bytes.Replace(z, k2, v, -1)
+	}
+}
+
+
+// 1000000	1218 ns/op
+// alexflint/go-restructure/regex
+func BenchmarkRxRestReplaceAllString(b *testing.B) {
+	z := `azaza ${Name} zazaz. `
+	rx := regex.MustCompile(`\$\{([\w]{1,1000})\}`)
+	for i := 0; i < b.N; i++ {
+		rx.ReplaceAllString(z,`Mary`)
+	}
+}
+
+// 1000000	1210 ns/op
+// alexflint/go-restructure/regex
+func BenchmarkRxRestReplaceAll(b *testing.B) {
+	z := []byte(`azaza ${Name} zazaz. `)
+	v := []byte(`Mary`)
+	rx := regex.MustCompile(`\$\{([\w]{1,1000})\}`)
+	for i := 0; i < b.N; i++ {
+		rx.ReplaceAll(z,v)
+	}
+}
+
+// 500000	2799 ns/op
+// dlclark/regexp2
+func BenchmarkRx2Replace(b *testing.B) {
+	z := `azaza ${Name} zazaz. `
+	rx := regexp2.MustCompile(`\$\{([\w]{1,1000})\}`,0)
+	for i := 0; i < b.N; i++ {
+		rx.Replace(z,`Mary`,-1,-1)
 	}
 }
