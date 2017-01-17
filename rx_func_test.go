b2bb8163f03e243381ff3929ffd3fc37a9f8bf1d
eugene <eugenemcmad@gmail.com>
2017-01-17 16:44:04 +0300

    +

--- rx_func_test.go
+++ rx_func_test.go
@@ -31,30 +31,55 @@ var (
         </div> <br/>
 <a href="${unsub}" target="_blank" style="color:#00addb; text-decoration: none;">Unsubscribe</a></span>
 <a href="https://rm.regium.com/close?a=b&c=end" target="_blank" > close me </a>`
+
+	tstTxt = `
+boddy start
+http://rm.regium.com/10101010 http://rm.regium.com/20202020 (http://rm.regium.com/30303030) http://rm.regium.com/40404040
+www.wrong-uri.com
+https://rm.regium.com/50505050
+end http://rm.regium.com/60606060`
 )
 
 // go test -v xr/snippets -bench ^Benchmark_RXLHFN_ -run ^$
 
-// 3321 ns/op
+// 65267 ns/op
 func Benchmark_RXLHFN_RxFn(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		setSimpleLinksToHtml_RX(tstHtml, "")
 	}
 }
 
-// 3321 ns/op
+// 14791 ns/op
 func Benchmark_RXLHFN_UtFn(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		setSimpleLinksToHtml_UT(tstHtml, "")
 	}
 }
 
-func TestRcFunc(t *testing.T) {
-
+func TestLHtmlFunc(t *testing.T) {
 	fmt.Printf("setSimpleLinksToHtml_RX(): %v\n\n", setSimpleLinksToHtml_RX(tstHtml, ""))
-
 	fmt.Printf("setSimpleLinksToHtml_UT(): %v\n\n", setSimpleLinksToHtml_UT(tstHtml, ""))
+}
 
+// go test -v xr/snippets -bench ^Benchmark_RXLTFN_ -run ^$
+
+// 70231 ns/op
+func Benchmark_RXLTFN_RxFn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		setSimpleLinksToText_RX(tstHtml, "")
+	}
+}
+
+// 10769 ns/op
+func Benchmark_RXLTFN_UtFn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		setSimpleLinksToText_UT(tstHtml, "")
+	}
+}
+
+func TestLTextFunc(t *testing.T) {
+	fmt.Printf("setSimpleLinksToText_RX(): %v\n\n", setSimpleLinksToText_RX(tstTxt, ""))
+	fmt.Printf("setSimpleLinksToText_UT(): %v\n\n", setSimpleLinksToText_UT(tstTxt, ""))
 }
 
 // Set open/simple redirect & src links to html content. TODO: OPTIMIZ 0000 WINNER
@@ -85,16 +110,9 @@ func setSimpleLinksToHtml_RX( /*trackParams trackParams,*/ str, trackDomain stri
 
 // ` (href|src)="https?://([a-z0-9+-~_/.:;=#%$@&{}]{5,1000})"`
 func setSimpleLinksToHtml_UT(str, trackDomain string) string {
-
-	// find start: (href|src)="https?://(
-	// find end:   "
-	// get action
-	// get url origin
-	// get url result
-	// replace
 	var res, url string
 	var l0, l1, r0, actionId int
-	var in, isSrc bool
+	var in bool
 	for i := 0; i < len(str)-14; i++ {
 
 		switch {
@@ -103,7 +121,6 @@ func setSimpleLinksToHtml_UT(str, trackDomain string) string {
 			i += 14
 			l1 = l0 + 7
 			in = true
-			isSrc = false
 			actionId = ActionRedirect
 			break
 		case str[i:i+14] == ` href="http://`:
@@ -111,7 +128,6 @@ func setSimpleLinksToHtml_UT(str, trackDomain string) string {
 			i += 13
 			l1 = l0 + 7
 			in = true
-			isSrc = false
 			actionId = ActionRedirect
 			break
 		case str[i:i+14] == ` src="https://`:
@@ -119,7 +135,6 @@ func setSimpleLinksToHtml_UT(str, trackDomain string) string {
 			i += 13
 			l1 = l0 + 6
 			in = true
-			isSrc = true
 			actionId = ActionNo
 			break
 		case str[i:i+13] == ` src="http://`:
@@ -127,7 +142,6 @@ func setSimpleLinksToHtml_UT(str, trackDomain string) string {
 			i += 12
 			l1 = l0 + 6
 			in = true
-			isSrc = true
 			actionId = ActionNo
 			break
 		case in && str[i] == ' ': // Error - skip pattern
@@ -138,14 +152,10 @@ func setSimpleLinksToHtml_UT(str, trackDomain string) string {
 		case in && str[i:i+2] == `" `: // Pattern complete
 			in = false
 			url = str[l1:i]
-			if isSrc {
-				res += str[r0:l0] + ` src="http://` + "[START[" + url + "]END]"
-			} else {
-				res += str[r0:l0] + ` href="http://` + "[START[" + url + "]END]"
-			}
+			res += str[r0:l1] + "[START[" + url + "]END]"
 
 			r0 = i
-			fmt.Printf("'%v' l:%d, %d aid:%v \n", url, l0, l1, actionId)
+			//fmt.Printf("'%v' l:%d, %d aid:%v \n", url, l0, l1, actionId)
 			break
 		default:
 			continue
@@ -180,18 +190,46 @@ func setSimpleLinksToText_RX( /*trackParams trackParams,*/ str, trackDomain stri
 
 // Set open/simple redirect & src links to text content.
 // `([\s\(]|^)https?://([a-z0-9+-~_/.:;=#%$@&{}]{5,1000})([\n\s\)]|$)`
-func setSimpleLinksToText_UT( /*trackParams trackParams,*/ str, trackDomain string) string {
-	return rxHttpUrlPtrnInText.ReplaceAllStringFunc(str, func(src string) string {
+func setSimpleLinksToText_UT(str, trackDomain string) string {
+	// `([\s\(]|^)https?://([a-z0-9+-~_/.:;=#%$@&{}]{5,1000})([\n\s\)]|$)`
+	//start := rxHttpUrlPtrnInText.ReplaceAllString(src, `$1`) // pre ([\s\(]|^)  ==       [' ', '(', '{START_STRING}']
+	//url := rxHttpUrlPtrnInText.ReplaceAllString(src, `$2`)   // pure url
+	//end := rxHttpUrlPtrnInText.ReplaceAllString(src, `$3`)   // end ([\n\s\)]|$)== ['\n', ' ', ')', '{END_STRING}']
+	var res, url string
+	var l0, r0 int
+	var in bool
+	for i := 0; i < len(str); i++ {
+		switch {
+		case i < len(str)-8 && str[i:i+8] == `https://`:
+			l0 = i
+			i += 7
+			in = true
+			break
+		case i < len(str)-7 && str[i:i+7] == `http://`:
+			l0 = i
+			i += 6
+			in = true
+			break
+		case in && (str[i] == ' ' || str[i] == ')' || str[i] == '\n'): // Pattern complete
+			in = false
+			url = str[l0:i]
+			res += str[r0:l0] + "[START[" + url + "]END]"
+			r0 = i
+			//fmt.Printf("[%v] l:%d from %d\n", url, l0, len(str))
+			break
+		case in && i == len(str)-1: // Pattern complete
+			in = false
+			url = str[l0:]
+			res += str[r0:l0] + "[START[" + url + "]END]"
+			r0 = len(str)
+			//fmt.Printf("[%v] l:%d from %d\n", url, l0, len(str))
+			break
+		default:
+			continue
+		}
+	}
 
-		start := rxHttpUrlPtrnInText.ReplaceAllString(src, `$1`) // pre ([\s\(]|^)  ==       [' ', '(', '{START_STRING}']
-		url := rxHttpUrlPtrnInText.ReplaceAllString(src, `$2`)   // pure url
-		end := rxHttpUrlPtrnInText.ReplaceAllString(src, `$3`)   // end ([\n\s\)]|$)== ['\n', ' ', ')', '{END_STRING}']
+	res += str[r0:]
 
-		//newLink, err := getActLink(trackParams, ActionRedirect, trackDomain, url)
-		newLink := "HTTP://" + url + "-ATTR-IS-" + "ActionRedirect" + "-END"
-
-		repLink := start + newLink + end
-
-		return repLink
-	})
+	return res
 }
